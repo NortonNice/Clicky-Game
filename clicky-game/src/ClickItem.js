bc087a1188b4437f591b4ccee54d5415c73b4db9
Norton Nice <nortonnice@gmail.com>
2019-01-05 21:01:30 -0800

    hardcoded first image into component ClickItem

---
+++ clicky-game/src/ClickItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+const ClickItem = (props) => {
+    return (
+        <div>
+        <p>SHOSHDOIHSDOSIHDOSHD</p>
+        <img src={require('./assets/images/cash.jpg')} className = "clickItem" alt = "CASH " />
+        </div> 
+    )
+}
+
+export default ClickItem;
