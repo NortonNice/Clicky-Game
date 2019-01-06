935f2b23c5faba18180a1dae44d307f2bf3b6c14
Norton Nice <nortonnice@gmail.com>
2019-01-05 22:26:49 -0800

    added bootstrap, reactstrap, modified index with bootstrap, finished grid

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -5,14 +5,41 @@ import ClickItem from "./ClickItem";
 class App extends Component {
   render() {
     return (
-      <div>
-      <ClickItem />
-        
-      
+      <div class="container">
+        <div class="row">
+          <div class="col-sm">
+            <ClickItem />
+            <ClickItem />
+            <ClickItem />
+          </div>
+          <div class="col-sm">
+            <ClickItem />
+            <ClickItem />
+            <ClickItem />
+          </div>
+          <div class="col-sm">
+            <ClickItem />
+            <ClickItem />
+            <ClickItem />
+          </div>
+          <div class="col-sm">
+            <ClickItem />
+            <ClickItem />
+            <ClickItem />
+          </div>
+
+        </div>
       </div>
+
     );
   }
 }
 
 export default App;
+
+
+
+
+
+
 
--- clicky-game/src/ClickItem.js
+++ clicky-game/src/ClickItem.js
@@ -5,7 +5,6 @@ import './App.css';
 const ClickItem = (props) => {
     return (
         <div>
-        <p>SHOSHDOIHSDOSIHDOSHD</p>
         <img src={require('./assets/images/cash.jpg')} className = "clickItem" alt = "CASH " />
         </div> 
     )
